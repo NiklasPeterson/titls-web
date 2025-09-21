84408416b401643eb2e0ac9c016f7b13d97ac1e4
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-09-21 12:03:59 +0200

    Update old domain reference to use new titls.app address

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
 
             <div className='flex flex-wrap gap-4 items-center justify-center md:justify-start'>
               <DownloadButton />
-              <a href='mailto:titls@niklaspeterson.com' className='text-base font-medium rounded-lg px-5 py-3 flex gap-1 items-center text-zinc-950 bg-zinc-100 border border-zinc-200 hover:bg-zinc-200 dark:text-white dark:bg-zinc-900 dark:border-zinc-800 dark:hover:bg-zinc-800'>
+              <a href='mailto:titls.app' className='text-base font-medium rounded-lg px-5 py-3 flex gap-1 items-center text-zinc-950 bg-zinc-100 border border-zinc-200 hover:bg-zinc-200 dark:text-white dark:bg-zinc-900 dark:border-zinc-800 dark:hover:bg-zinc-800'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
                 </svg>
